b6c4a1ab3fc6820ee329b2cd3bba8a398e3dbcfc
akshatagaonkar <agaonkar38@gmail.com>
2015-07-09 17:34:36 +0530

    Final Submission Commit

---
+++ 1342OnlineTest/src/voting/RegisterTest.java
@@ -0,0 +1,14 @@
+package voting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RegisterTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
